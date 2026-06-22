541cf68ee81e36ed932e49de5a7655c1bd1fcd8b
1337Ghostface <wu.portugal@gmail.com>
2026-06-22 17:17:43 +0100

    Refactor exercise selection and improve user prompts in ex1_18_06_2026.c; update binary file

--- ex1_18_06_2026.c
+++ ex1_18_06_2026.c
@@ -1,186 +1,236 @@
 #include<stdio.h>
+#include<stdbool.h>
 
 int main(){
 
-    /*1- Leitura e apresentação de valores
-    Lê 10 numeros inteiros para um vetor.
-    Mostra todos os valores introduzidos.*/
+    int opcao;
 
-    printf("Parte 1 -Leitura e apresentacao de valores. \n\n");
+    printf("Escolha o exercicio que quer fazer:\n");
+    printf("1 - Leitura e apresentacao de valores\n");
+    printf("2 - Soma dos elementos\n");
+    printf("3 - Maior e menor valor\n");
+    printf("4 - Media e notas positivas\n");
+    printf("5 - Contagem de pares e impares\n");
+    printf("6 - Pesquisa de um numero\n");
+    printf("7 - Inversao do vetor\n");
+    printf("Opcao: ");
+    scanf("%d", &opcao);
 
-    int vetor1[10];
+    switch(opcao){
 
-    for(int i=0;i<10;i++){
-        printf("Digite o numero %d:\n", (i+1));
-        scanf("%d", &vetor1[i]);
-    }
+        case 1:{
+            /*1- Leitura e apresentação de valores
+            Lê 10 numeros inteiros para um vetor.
+            Mostra todos os valores introduzidos.*/
 
-    printf("Os numeros inseridos foram: ");
-    
-    for(int i=0;i<10;i++){
-        printf("%d, ", vetor1[i]);
-    }
+            printf("Parte 1 -Leitura e apresentacao de valores. \n\n");
 
-    printf("\n\nParte 1 concluida. \n\n\n\n");
+            int vetor1[10];
 
-    /*2- Soma dos elementos:
-    Lê 8 números inteiros para um segundo vetor.
-    Calcula e apresenta a soma de todos os elementos.*/
-
-    printf("Parte 2 - Soma dos elementos. \n\n");
-
-    int vetor2[8], soma=0;
-
-    for(int i=0;i<8;i++){
-        printf("Digite o numero %d:\n", (i+1));
-        scanf("%d", &vetor2[i]);
-        soma += vetor2[i];
-    }
-
-    printf("A soma de todos os valores e de: %d", soma);
-
-    printf("\n\nParte 2 - concluida. \n\n\n\n");
-
-    /*3-Maior e menor valor:
-    Lê 10 números inteiros para um terceiro vetor.
-    Determina e apresenta: O maior valor, O menor valor, A posição onde aparece o maior, A posição onde aparece o menor.*/
-
-
-    printf("Parte 3 - Maior e menor valor. \n\n");
-
-    int vetor3[10], valMaior, valMenor, posMaior=0, posMenor=0;
-
-    for(int i=0;i<10;i++){
-        printf("Digite o numero %d:\n", (i+1));
-        scanf("%d", &vetor3[i]);
-    }
-
-    valMenor = vetor3[0];
-    valMaior = vetor3[0];
-
-    for(int i=0;i<10;i++){
-
-        if(vetor3[i] < valMenor){
-            valMenor = vetor3[i];
-            posMenor = i+1;
-        }
-
-        if(vetor3[i] > valMaior){
-            valMaior = vetor3[i];
-            posMaior = i+1;
-        }
-    }
-
-    printf("O maior valor e %d, e esta na posicao %d.\n", valMaior, posMaior);
-    printf("O menor valor e %d, e esta na posicao %d. \n", valMenor, posMenor);
-
-    printf("\n\nParte 3 - concluida. \n\n\n\n");
-
-    /*4-Média e notas positivas:
-    Lê 6 notas (valores reais ou inteiros) para um vetor.
-    Calcula e apresenta a média das notas.
-    Indica também quantas notas são positivas (>=10)*/
-
-    printf("Parte 4 - Media e notas positivas. \n\n");
-
-    float media=0;
-    int vetor4[6], positivas=0, somaMedia=0;
-
-    for(int i=0; i<6; i++){
-        printf("Digite a notas: \n");
-        scanf("%d", &vetor4[i]);
-
-            if(vetor4[i] >= 10){
-                positivas++;
+            for(int i=0;i<10;i++){
+                printf("Digite o numero %d:\n", (i+1));
+                scanf("%d", &vetor1[i]);
             }
-        somaMedia = somaMedia + vetor4[i];
-    }
-    media = media + somaMedia / 6;
 
-    printf("A media e de %.2f. E ha %d positivas.\n", media, positivas);
+            printf("Os numeros inseridos foram: ");
+            
+            for(int i=0;i<10;i++){
+                printf("%d, ", vetor1[i]);
+            }
 
-    printf("\n\nParte 4 - concluida. \n\n\n\n");
+            printf("\n\nParte 1 concluida.\n\n\n\n");
 
-    /*5-Contagem de pares e ímpares:
-    Lê 12 números inteiros para um vetor. indica quantos valores são pares e quantos são ímpares.*/
-
-    int vetor5[12], par=0, impar=0;
-
-    printf("Parte 5 - Contagem de pares e impares. \n\n");
-
-    for(int i=0;i<12;i++){
-        printf("Insira os numeros: \n");
-        scanf("%d", &vetor5[i]);
-        
-        if(vetor5[i] % 2 == 0){
-            par++;
+            break;
         }
-        else{
-            impar++;
+
+        case 2:{
+            /*2- Soma dos elementos:
+            Lê 8 números inteiros para um segundo vetor.
+            Calcula e apresenta a soma de todos os elementos.*/
+
+            printf("Parte 2 - Soma dos elementos. \n\n");
+
+            int vetor2[8], soma=0;
+
+            for(int i=0;i<8;i++){
+                printf("Digite o numero %d:\n", (i+1));
+                scanf("%d", &vetor2[i]);
+                soma += vetor2[i];
+            }
+
+            printf("A soma de todos os valores e de: %d", soma);
+
+            printf("\n\nParte 2 concluida.\n\n\n\n");
+
+            break;
+        }
+
+        case 3:{
+            /*3-Maior e menor valor:
+            Lê 10 números inteiros para um terceiro vetor.
+            Determina e apresenta: O maior valor, O menor valor, A posição onde aparece o maior, A posição onde aparece o menor.*/
+
+
+            printf("Parte 3 - Maior e menor valor. \n\n");
+
+            int vetor3[10], valMaior, valMenor, posMaior=1, posMenor=1;
+
+            for(int i=0;i<10;i++){
+                printf("Digite o numero %d:\n", (i+1));
+                scanf("%d", &vetor3[i]);
+            }
+
+            valMenor = vetor3[0];
+            valMaior = vetor3[0];
+
+            for(int i=0;i<10;i++){
+
+                if(vetor3[i] < valMenor){
+                    valMenor = vetor3[i];
+                    posMenor = i+1;
+                }
+
+                if(vetor3[i] > valMaior){
+                    valMaior = vetor3[i];
+                    posMaior = i+1;
+                }
+            }
+
+            printf("O maior valor e %d, e esta na posicao %d.\n", valMaior, posMaior);
+            printf("O menor valor e %d, e esta na posicao %d. \n", valMenor, posMenor);
+
+            printf("\n\nParte 3 - concluida. \n\n\n\n");
+
+            break;
+        }
+
+        case 4:{
+            /*4-Média e notas positivas:
+            Lê 6 notas (valores reais ou inteiros) para um vetor.
+            Calcula e apresenta a média das notas.
+            Indica também quantas notas são positivas (>=10)*/
+
+            printf("Parte 4 - Media e notas positivas. \n\n");
+
+            float media=0;
+            int vetor4[6], positivas=0, somaMedia=0;
+
+            for(int i=0; i<6; i++){
+                printf("Digite a notas: \n");
+                scanf("%d", &vetor4[i]);
+
+                    if(vetor4[i] >= 10){
+                        positivas++;
+                    }
+                somaMedia = somaMedia + vetor4[i];
+            }
+            media = media + somaMedia / 6;
+
+            printf("A media e de %.2f. E ha %d positivas.\n", media, positivas);
+
+            printf("\n\nParte 4 - concluida. \n\n\n\n");
+
+            break;
+        }
+
+        case 5:{
+            /*5-Contagem de pares e ímpares:
+            Lê 12 números inteiros para um vetor. indica quantos valores são pares e quantos são ímpares.*/
+
+            int vetor5[12], par=0, impar=0;
+
+            printf("Parte 5 - Contagem de pares e impares. \n\n");
+
+            for(int i=0;i<12;i++){
+                printf("Insira os numeros: \n");
+                scanf("%d", &vetor5[i]);
+                
+                if(vetor5[i] % 2 == 0){
+                    par++;
+                }
+                else{
+                    impar++;
+                }
+            }
+
+            printf("Tem %d pares e %d impar.\n", par, impar);
+
+            printf("\n\nParte 5 - concluida. \n\n\n\n");
+
+            break;
+        }
+
+        case 6:{
+            /*6-pesquisa de um número:
+            Lê 10 números inteiros para um vetor.
+            Pede ao utilizador um número a procurar.
+            Indica se o número existe no vetor e, caso exista, em que posição aparece.*/
+
+            int vetor6[10], num, posi=0;
+            bool flag = false;
+
+            printf("Parte 6 - Pesquisa de um numero. \n\n");
+
+            for(int i=0;i<10;i++){
+                printf("Insira os numeros: \n");
+                scanf("%d" , &vetor6[i]);
+            }
+
+            do{
+            printf("\nDigite o numero que procura: \n");
+            scanf("%d", &num);
+
+            for(int i=0;i<10;i++){
+                if(num == vetor6[i]){
+                    posi = i;
+                    flag = true;
+                }
+            }
+
+            if(flag==false){
+                printf("O numero nao foi encontrado.");
+            }
+            }while(flag==false);
+
+            if(flag==true){
+                printf("O numero %d, foi encontrado na posicao %d", num, (posi+1));
+            }
+
+            printf("\n\nParte 6 - concluida. \n\n\n\n");
+
+            break;
+        }
+
+        case 7:{
+            /*7-Inversão do vetor:
+            Lê 10 números inteiros para um vetor.
+            Mostra o vetor pela ordem inversa.*/
+
+            int vetor7[10];
+
+            printf("Parte 7 - Inverter vetor. \n\n");
+
+            for(int i=0;i<10;i++){
+                printf("Digite os numeros: \n");
+                scanf("%d", &vetor7[i]);
+            }
+
+            printf("Os numeros na ordem invertida sao:");
+
+            for(int i=9;i>=0;i--){
+                printf(" %d,", vetor7[i]);
+            }
+
+            printf("\n\nParte 7 - concluida. \n\n\n\n");
+
+            break;
+        }
+
+        default:{
+            printf("Opcao invalida.\n");
+            break;
         }
     }
-
-    printf("Tem %d pares e %d impar.\n", par, impar);
-
-    printf("\n\nParte 5 - concluida. \n\n\n\n");
-
-    /*6-pesquisa de um número:
-    Lê 10 números inteiros para um vetor.
-    Pede ao utilizador um número a procurar.
-    Indica se o número existe no vetor e, caso exista, em que posição aparece.*/
-
-    int vetor6[10], num, posi=0;
-    bool flag = false;
-
-printf("Parte 6 - Pesquisa de um numero. \n\n");
-
-    for(int i=0;i<10;i++){
-        printf("Insira os numeros: \n");
-        scanf("%d" , &vetor6[i]);
-    }
-
-    do{
-    printf("\nDigite o numero que procura: \n");
-    scanf("%d", &num);
-
-    for(int i=0;i<10;i++){
-        if(num == vetor6[i]){
-            posi = i;
-            flag = true;
-        }
-    }
-
-    if(flag==false){
-        printf("O numero nao foi encontrado.");
-    }
-    }while(flag=1=false);
-
-    if(flag==true){
-        printf("O numero %d, foi encontrado na posicao %d", num, (posi+1));
-    }
-
-    printf("\n\nParte 6 - concluida. \n\n\n\n");
-
-    /*7-Inversão do vetor:
-    Lê 10 números inteiros para um vetor.
-    Mostra o vetor pela ordem inversa.*/
-
-    int vetor7[10];
-
-    printf("Parte 7 - Inverter vetor. \n\n");
-
-    for(int i=0;i<10;i++){
-        printf("Digite os numeros: \n");
-        scanf("%d", &vetor7[i]);
-    }
-
-    printf("Os numeros na ordem invertida sao:");
-
-    for(int i=9;i>=0;i--){
-        printf(" %d,", vetor7[i]);
-    }
-
-    printf("\n\nParte 7 - concluida. \n\n\n\n");
 
     return 0;
 }
