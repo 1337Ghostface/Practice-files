120efc08f894a8e90d75b4bd12867d84912b5f50
1337Ghostface <wu.portugal@gmail.com>
2026-07-06 20:20:11 +0100

    Remove unnecessary line breaks for improved readability in decimal to binary conversion

--- dec_bin.c
+++ dec_bin.c
@@ -8,29 +8,21 @@ int main() {
     printf("Insere um numero decimal entre 0 e 127: "); //usando o vetor apenas ate 64, so permite que o maior numero inserido seja 127
 
     if (scanf("%d", &decimal) != 1) { //ler numero inserido
-
         printf("Erro: tens de inserir um numero inteiro.\n"); //se o utilizador nao meter numero inteiro, da erro
-
         return 1; //termina o programa
     }
 
     if (decimal < 0 || decimal > 127) { //verificar se o numero esta dentro do limite
-
         printf("Erro: o numero tem de estar entre 0 e 127.\n"); //se o numero for menor que 0 ou maior que 127, dar erro
-
         return 1; //termina o programa
     }
 
     for (int i = 0; i < 7; i++) { //percorrer os valores 64, 32, 16, 8, 4, 2, 1 no vetor
-
         if (decimal >= valores[i]) { //se o numero ainda da para ser retirado do valor atual
-
             printf("1"); //mostrar 1
-
             decimal = decimal - valores[i]; //subtrair o valor
 
         } else { //se nao
-
             printf("0"); //mostrar 0
         }
     }
