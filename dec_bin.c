1691ba6f02630737c685e62b39f39bf29b55b7fa
1337Ghostface <wu.portugal@gmail.com>
2026-07-06 20:16:59 +0100

    Refactor variable declarations for clarity and scope in decimal to binary conversion

--- dec_bin.c
+++ dec_bin.c
@@ -3,9 +3,7 @@
 int main() {
 
     int decimal;
-    int original;
     int valores[7] = {64, 32, 16, 8, 4, 2, 1}; //defino os valores do vetor baseado no metodo que uso
-    int i;
 
     printf("Insere um numero decimal entre 0 e 127: "); //usando o vetor apenas ate 64, so permite que o maior numero inserido seja 127
 
@@ -23,7 +21,7 @@ int main() {
         return 1; //termina o programa
     }
 
-    for (i = 0; i < 7; i++) { //percorrer os valores 64, 32, 16, 8, 4, 2, 1 no vetor
+    for (int i = 0; i < 7; i++) { //percorrer os valores 64, 32, 16, 8, 4, 2, 1 no vetor
 
         if (decimal >= valores[i]) { //se o numero ainda da para ser retirado do valor atual
 
